4d6f73d891a596569015159370eb58da758b6da5
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-02-13 16:04:52 -0500

    updated home to map server fetch.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,14 @@ import ReactPlayer from 'react-player';
 import { ABCcalls, ABCvideo, ABCuserInfo, ABCtoken } from '../App';
 import dbCall from "../helpers/Environments";
 
-interface highlightedVideosProps {
-    id: number;
-    name: string;
-    playerHighlighted: string;
-    href: string;
-    tag: string;
-    src: string;
+interface videoState {
+	videoId: string;
+	videoTitle: string;
+	videoLink: string;
+	videoOwner: string;
+	videoOwnerId: string;
+	videoOwnerUsername: string;
+	videoPostsArray: string[];
 }
 
 interface videoProps {
@@ -39,22 +40,6 @@ interface videoProps {
 	// adminDelete: boolean;
 }
 
-type videoProperties = {
-    id: string;
-    videoTitle: string;
-    videoLink: string;
-    // thumbnailImage: string;
-    // playersHighlighted: playersHighlightedArray[];
-    // teamsFeatured: teamsFeaturedArray[];
-    // tags: tagsArray[];
-    // gameDate: Date;
-    // nbaSeason: string;
-    // isPlayoffs: boolean;
-    // clutch: boolean;
-    // adminHighlighted: boolean;
-    // adminDelete: boolean;
-}
-
 // type playersHighlightedArray = {
 //     id: number;
 //     playerName: string;
@@ -72,87 +57,91 @@ type videoProperties = {
 
 export default class Home extends Component<
 	videoProps,
-	{ highlightedVideos: highlightedVideosProps[] }
+	videoState
 > {
 	constructor(props: videoProps) {
 		super(props);
 		this.state = {
-			highlightedVideos: [
-				{
-					id: 1,
-					name: "Shaq and Embiid",
-					playerHighlighted: "Talk Show",
-					href: "https://streamable.com/pfamf9",
-					tag: "NBA",
-					src: "https://streamable.com/pfamf9",
-				},
-				{
-					id: 2,
-					name: "Sdd",
-					playerHighlighted: "Talk Show",
-					href: "https://streamable.com/pfamf9",
-					tag: "NBA",
-					src: "https://streamable.com/pfamf9",
-				},
-				{
-					id: 3,
-					name: "Sdd",
-					playerHighlighted: "Talk Show",
-					href: "https://streamable.com/pfamf9",
-					tag: "NBA",
-					src: "https://streamable.com/pfamf9",
-				},
-				{
-					id: 4,
-					name: "Sdd",
-					playerHighlighted: "Talk Show",
-					href: "https://streamable.com/pfamf9",
-					tag: "NBA",
-					src: "https://streamable.com/pfamf9",
-				},
-				{
-					id: 5,
-					name: "Sdd",
-					playerHighlighted: "Talk Show",
-					href: "https://streamable.com/pfamf9",
-					tag: "NBA",
-					src: "https://streamable.com/pfamf9",
-				},
-				{
-					id: 6,
-					name: "Sdd",
-					playerHighlighted: "Talk Show",
-					href: "https://streamable.com/pfamf9",
-					tag: "NBA",
-					src: "https://streamable.com/pfamf9",
-				},
-				// More videos...
-            ],
-            
-        }
-        this.fetchVideos = this.fetchVideos.bind(this);
+			videoId: "",
+			videoTitle: "",
+			videoLink: "",
+			videoOwner: "",
+			videoOwnerId: "",
+			videoOwnerUsername: "",
+			videoPostsArray: [],
+		};
+		this.fetchVideos = this.fetchVideos.bind(this);
+		// this.videoPostArrayRender = this.videoPostArrayRender.bind(this);
 	}
-
-	//! fetching all videos, regardless of validation.
-	//! sort by newest first.
-	fetchVideos = async () => {
-		const response = await fetch(`${dbCall}/videos/content/all`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-		const data = await response.json();
-		console.log(data);
-		this.props.setResponseStatus(response.status);
-		this.props.setErrorMessage(data.errorMessage);
-	};
 
 	componentDidMount() {
 		this.fetchVideos();
 	}
 
-	render() {
+	//! returns [] of videos with following properties:
+	//? videoID, videoTitle, videoLink.
+	//! sort by newest first.
+	fetchVideos = async () => {
+		await fetch(`${dbCall}/videos/content/all`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					return response.json();
+				} else {
+					throw new Error("Error fetching videos");
+				}
+			})
+			.then((responseJson) => {
+				console.log("Home :", responseJson);
+				this.setState({
+					// set the data to a array of videos
+					videoPostsArray: responseJson.allVideos,
+				});
+				console.log("Videos: ", this.state.videoPostsArray);
+			})
+			.catch((err) => {
+				this.props.setErrorMessage(err.message);
+				console.log("Error with fetchVideos: ", err);
+			});
+	};
+
+	//! returns [] of videos with following properties:
+	//? videoID, videoTitle, videoLink.
+	//! sort by newest first.
+	videoPostArrayRender = () => {
+		return this.state.videoPostsArray?.map((videos: any, index: number) => {
+			return (
+				<li key={videos.videoID} className="pb-5 ">
+					<div className=" space-y-2 space-x-2 shadow-lg rounded-lg  bg-red-400 ">
+						<div className="">
+							<ReactPlayer
+								width={"100%"}
+								url={videos.videoLink}
+								id="react-player"
+								className="rounded-lg "
+							/>
+						</div>
+
+						<div className="space-y-2 ">
+							<div className="text-lg -mt-4 pb-5 leading-6 font-medium space-y-1">
+								<h3>{videos.videoTitle}</h3>
+								<p className="text-indigo-600">
+									{videos.videoOwnerUsername}
+								</p>
+							</div>
+						</div>
+					</div>
+				</li>
+			);
+		})
+	};
+
+
+	render(): React.ReactNode {
 		return (
 			<div className="bg-white">
 				<div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-16">
@@ -168,26 +157,7 @@ export default class Home extends Component<
 							role="list"
 							className="-mt-7 scroll-px-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
 						>
-							{this.state.highlightedVideos.map(
-								(video: highlightedVideosProps) => (
-									<li key={video.id} className="pb-5 ">
-										<div className=" space-y-2 space-x-2 shadow-lg rounded-lg  bg-red-400 ">
-											<div className="">
-												<ReactPlayer
-													width={"100%"}
-													url={video.src}
-													id="react-player"
-													className="rounded-lg "
-												/>
-											</div>
-
-											<div className="space-y-2 ">
-												<div className="text-lg -mt-4 pb-5 leading-6 font-medium space-y-1">
-													<h3>{video.name}</h3>
-													<p className="text-indigo-600">
-														{video.playerHighlighted}
-													</p>
-												</div>
+							{this.videoPostArrayRender()}
 												{/* <ul role="list" className="flex space-x-5">
                             <li>
                             <a href={video.team} className="text-gray-400 hover:text-gray-500">
@@ -200,17 +170,14 @@ export default class Home extends Component<
                             <li>
                             could put a second logo here of something for category or list out names
                             </li>
-                        </ul> */}
-											</div>
-										</div>
-									</li>
-								)
-							)}
+							</ul> */
+							}
 						</ul>
 					</div>
 				</div>
-			</div>
+				</div>
 		);
+		}
 	}
-}
+			
 
